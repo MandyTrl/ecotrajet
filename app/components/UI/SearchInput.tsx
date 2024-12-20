64743e83d964e007122e9bed585ae096cd202503
MandyTrl <thorelmandy@gmail.com>
2024-12-20 14:11:48 +0100

    handle onBlur input + add animation on dropdown

--- app/components/UI/SearchInput.tsx
+++ app/components/UI/SearchInput.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { UserLocationContext } from "@/app/utils/Context"
 import { ChangeEvent, useContext, useState } from "react"
+import clsx from "clsx"
+import { UserLocationContext } from "@/app/utils/Context"
 
 type InputProps = {
 	name: string
@@ -67,25 +68,31 @@ export const SearchInput = ({
 				placeholder={placeholder}
 				minLength={minLength}
 				type={type}
-				className="w-full p-2 mt-1 rounded-md bg-emerald-100/50 placeholder:text-emerald-700 placeholder:text-sm border-b border-transparent focus:border-emerald-500 outline-none ease-in-out duration-150 transition-color"
+				className="w-full py-2 px-3 mt-1 rounded-md bg-emerald-100/50 placeholder:text-emerald-700 placeholder:text-sm border-b border-transparent focus:border-emerald-500 outline-none ease-in-out duration-150 transition-color"
 				value={selectedValue}
 				onChange={(e) => handleInputChange(e)}
+				onBlur={() => setIsOpen(false)}
 			/>
 
-			{isOpen && (
-				<ul className="p-2" role="listbox">
-					{suggestions.map((city, index) => (
-						<li
-							key={index}
-							className="hover:text-emerald-700 cursor-pointer"
-							role="option"
-							aria-selected={false} // Obligatoire pour l'accessibilité
-							onClick={() => handleCityClick(city)}>
-							{city.name}
-						</li>
-					))}
-				</ul>
-			)}
+			<ul
+				role="listbox"
+				className={clsx(
+					isOpen && minCar
+						? "opacity-100 translate-y-0 max-h-96"
+						: "opacity-0 translate-y-4 max-h-0",
+					"p-2 shadow rounded-md bg-transparent mt-1 ring-1 ring-slate-200/40 transition-all duration-500 ease-out transform"
+				)}>
+				{suggestions.map((city, index) => (
+					<li
+						key={index}
+						className="hover:text-emerald-700 cursor-pointer my-[2px]"
+						role="option"
+						aria-selected={false} // Obligatoire pour l'accessibilité
+						onClick={() => handleCityClick(city)}>
+						{city.name}
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
