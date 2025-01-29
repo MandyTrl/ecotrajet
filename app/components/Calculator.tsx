62a27009ccea5363e5ef0666b67ad6ae39d9e430
MandyTrl <thorelmandy@gmail.com>
2025-01-29 12:44:23 +0100

    refacto clean Calculator

--- app/components/Calculator.tsx
+++ app/components/Calculator.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useContext } from "react"
+import { useContext } from "react"
 import { calculateCarbonEmission } from "../utils/carbonCalculator"
 import { Transport, TransportMode } from "../utils/types"
 import Button from "./UI/Buttons/Button"
@@ -9,47 +9,35 @@ import { CoordinatesContext, SummaryContext } from "../utils/Context"
 import { useDistanceCalculator } from "../utils/hooks/useDistanceCalculator"
 
 export const Calculator = () => {
-	const { coordinates, handleCoordinates } = useContext(CoordinatesContext)
+	const { coordinates } = useContext(CoordinatesContext)
 	const { summary, updateSummary } = useContext(SummaryContext)
 	const { availableModes } = useDistanceCalculator(coordinates, updateSummary)
 
 	const hasMissingData: boolean = !(
-		coordinates.from ||
-		coordinates.to ||
+		coordinates.from &&
+		coordinates.to &&
 		summary.transport
 	)
 
-	useEffect(() => {
-		if (!coordinates.from || !coordinates.to) {
-			resetData()
-		}
-	}, [coordinates.from, coordinates.to])
-
-	const resetData = () => {
-		if (hasMissingData) return
-		handleCoordinates({
-			from: coordinates.from || null,
-			to: coordinates.to || null,
-		})
-		updateSummary({ distance: 0, carbonEmission: 0, transport: null })
-	}
-
 	const handleCalculate = async () => {
-		if (!coordinates.from || !coordinates.to || !summary.transport) {
+		if (hasMissingData) {
+			updateSummary({ distance: 0, carbonEmission: 0, transport: null })
 			console.error("Missing input data")
 			return
 		}
 
-		const from = `${coordinates.from.lon},${coordinates.from.lat}`
-		const to = `${coordinates.to.lon},${coordinates.to.lat}`
+		const from = `${coordinates.from!.lon},${coordinates.from!.lat}`
+		const to = `${coordinates.to!.lon},${coordinates.to!.lat}`
 
-		if (summary.transport.type === TransportMode.Plane) {
+		const carbonEmission = calculateCarbonEmission(
+			summary.distance,
+			summary.transport!.type,
+			summary.passengers
+		)
+
+		if (summary.transport!.type === TransportMode.Plane) {
 			updateSummary({
-				carbonEmission: calculateCarbonEmission(
-					summary.distance,
-					summary.transport.type,
-					summary.passengers
-				),
+				carbonEmission: carbonEmission,
 				isSummaryVisible: true,
 			})
 		} else {
@@ -62,7 +50,7 @@ export const Calculator = () => {
 					body: JSON.stringify({
 						from,
 						to,
-						mode: summary.transport.type,
+						mode: summary.transport!.type,
 						distance: summary.distance,
 					}),
 				})
@@ -72,11 +60,7 @@ export const Calculator = () => {
 				if (response.ok) {
 					updateSummary({
 						distance: summary.distance,
-						carbonEmission: calculateCarbonEmission(
-							summary.distance,
-							summary.transport.type,
-							summary.passengers
-						),
+						carbonEmission: carbonEmission,
 						isSummaryVisible: true,
 					})
 				} else {
